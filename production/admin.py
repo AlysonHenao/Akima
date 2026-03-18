4dde18849ab354eaff0fd3ea95ee46a4a50f8af9
AlysonH17 <alyson.uni2@gmail.com>
2026-03-17 20:05:31 -0500

    Client and administrator header separation

--- production/admin.py
+++ production/admin.py
@@ -1,8 +1,8 @@
 from django.contrib import admin
-from .models import Supply, SupplyColorProduct, EmployeeInventory, ProductionTask, TaskSupply
+from .models import Supply, SupplyColorProduct, EmployeeInventory, ProductionTask, SupplyTask
 
 admin.site.register(Supply)
 admin.site.register(SupplyColorProduct)
 admin.site.register(EmployeeInventory)
 admin.site.register(ProductionTask)
-admin.site.register(TaskSupply)
+admin.site.register(SupplyTask)
